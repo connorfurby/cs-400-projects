a79e444c20d3151f5eff9315f6e77f2ba47525fb
connorfurby <111527458+connorfurby@users.noreply.github.com>
2026-01-27 14:02:31 -0600

    Set up

--- P101.BinarySearchTree/BinarySearchTree.java
+++ P101.BinarySearchTree/BinarySearchTree.java
@@ -1,3 +1,45 @@
-public class BinarySearchTree {
-    
+public class BinarySearchTree<T extends Comparable<T>> implements SortedCollection<T> {
+    /**
+     * Inserts a new data value into the sorted collection.
+     * @param data the new value being inserted
+     * @throws NullPointerException if data argument is null, we do not allow
+     * null values to be stored within a SortedCollection
+     */
+    public void insert(T data) throws NullPointerException {
+        
+    }
+
+    /**
+     * Check whether data is stored in the tree.
+     * @param find the value to check for in the collection
+     * @return true if the collection contains data one or more times, 
+     * and false otherwise
+     */
+    public boolean contains(Comparable<T> find) {
+
+    }
+
+    /**
+     * Counts the number of values in the collection, with each duplicate value
+     * being counted separately within the value returned.
+     * @return the number of values in the collection, including duplicates
+     */
+    public int size() {
+
+    }
+
+    /**
+     * Checks if the collection is empty.
+     * @return true if the collection contains 0 values, false otherwise
+     */
+    public boolean isEmpty() {
+
+    }
+
+    /**
+     * Removes all values and duplicates from the collection.
+     */
+    public void clear() {
+        
+    }
 }
